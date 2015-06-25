d24b9647bf40048d0c60840c926cb78b66df1470
Fred Block <info@fredblock.com>
2015-06-25 06:49:19 -0600

    Add maps controller.
    new file:   app/assets/javascripts/maps.coffee
    new file:   app/assets/stylesheets/maps.scss
    new file:   app/controllers/maps_controller.rb
    new file:   app/helpers/maps_helper.rb
    new file:   app/views/maps/index.html.erb
    modified:   config/routes.rb
    new file:   test/controllers/maps_controller_test.rb

---
+++ app/controllers/maps_controller.rb
@@ -0,0 +1,14 @@
+class MapsController < ApplicationController
+  def index
+    @mapdata = if params[:lat] && params[:lon]
+      Event.get_json_map_data(location: { lat: params[:lat].to_s, lon: params[:lon].to_s })
+    else
+      Event.get_json_map_data
+    end
+
+    respond_to do |format|
+      format.html {}
+      format.json { render json: @mapdata }
+    end
+  end
+end
