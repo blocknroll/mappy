69cd52d70cd57f4bc964c63e2379a6188e5c4d42
Fred Block <info@fredblock.com>
2015-06-25 06:53:46 -0600

    Update 'maps_controller_test.rb'.
    modified:   test/controllers/maps_controller_test.rb
    new file:   test/vcr_cassettes/event_get_json_map_data.yml

--- test/controllers/maps_controller_test.rb
+++ test/controllers/maps_controller_test.rb
@@ -2,8 +2,11 @@ require 'test_helper'
 
 class MapsControllerTest < ActionController::TestCase
   test "should get index" do
-    get :index
-    assert_response :success
+    VCR.use_cassette("event#get_json_map_data") do
+      get :index
+      assert_response :success
+    end
   end
 
 end
+  
