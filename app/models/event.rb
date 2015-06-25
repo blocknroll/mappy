32d4200ff2b152a6200eac7ad69c8bdf21c8a3ef
Fred Block <info@fredblock.com>
2015-06-25 06:48:11 -0600

    Add 'get_json_map_data' method to 'event.rb'.
    modified:   app/models/event.rb
    modified:   config/routes.rb

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < OpenStruct
 
   def self.service
-    @service ||= EventfulService.new
+    @service ||= EventfulService.new  # get (or establish) a client connection to Eventful
   end
 
   def self.find_by(location)
@@ -9,5 +9,32 @@ class Event < OpenStruct
       Event.new(event)
     end
   end
+
+  def self.get_json_map_data(location: { lat: "39.750081", lon: "-104.999703" } )
+    service.get_events(location).map do |event|  # go to EventfulService > events, return the parsed hash, and map the Events data a new array
+      geo_data(event)                            # create new 'geo_data' objects for each Event (in private method below)
+    end
+  end
+
+
+
+      private
+
+              def self.geo_data(event)  # creates GeoJSON data with each event
+                {
+                    type: 'Feature',
+                    geometry: {
+                      type: 'Point',
+                      coordinates: [ event[:longitude], event[:latitude] ]
+                    },
+                    properties: {
+                      title:      event[:title],
+                      venue_name: event[:venue_name],
+                      url:        event[:url],
+                      :'marker-color' => '#F38E19',
+                      :'marker-size' => 'small'
+                    }
+                }
+              end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
-  root 'application#hello'
+  root      'maps#index'
+  resources :maps, only: [:index]
 
 end
